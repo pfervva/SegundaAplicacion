76ccaca230abef05b71923c9ecbd69b8d082eb2d
Pedro <pfernandezgonzalez@vva.safanet.es>
2023-12-03 17:41:28 +0100

    Version 0.3.3 - Añadimos logica para el Switch en los ajustes

--- app/src/main/java/com/example/myapplication/Ajustes.kt
+++ app/src/main/java/com/example/myapplication/Ajustes.kt
@@ -1,15 +1,16 @@
 package com.example.myapplication
-
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 
 class Ajustes : AppCompatActivity() {
 
     private lateinit var editTextNombreJugador: EditText
     private lateinit var editTextNumeroTiradas: EditText
+    private lateinit var switchDragonBall: Switch
     private lateinit var btnSaveConfiguracion: Button
     private lateinit var sharedPreferences: SharedPreferences
 
@@ -19,31 +20,37 @@ class Ajustes : AppCompatActivity() {
 
         editTextNombreJugador = findViewById(R.id.editTextNombreJugador)
         editTextNumeroTiradas = findViewById(R.id.editTextNumeroTiradas)
+        switchDragonBall = findViewById(R.id.switchCartaONumero)
         btnSaveConfiguracion = findViewById(R.id.btnGuardarConfiguracion)
 
         sharedPreferences = getSharedPreferences("configuracion", MODE_PRIVATE)
 
-        // Cargar configuración actual y mostrarla en los EditText
+        // Cargar configuración actual y mostrarla en los EditText y Switch
         val nombreJugador = sharedPreferences.getString("nombreJugador", "") ?: ""
         val numeroTiradas = sharedPreferences.getInt("numeroTiradas", 5)
+        val dragonBallActivado = sharedPreferences.getBoolean("dragonBallActivado", false)
+
         editTextNombreJugador.setText(nombreJugador)
         editTextNumeroTiradas.setText(numeroTiradas.toString())
+        switchDragonBall.isChecked = dragonBallActivado
 
         btnSaveConfiguracion.setOnClickListener {
             // Guardar la configuración en SharedPreferences al hacer clic en el botón
             val nuevoNombreJugador = editTextNombreJugador.text.toString()
             val nuevoNumeroTiradas = editTextNumeroTiradas.text.toString().toIntOrNull() ?: 5
-            guardarConfiguracion(nuevoNombreJugador, nuevoNumeroTiradas)
+            val nuevoDragonBallActivado = switchDragonBall.isChecked
+            guardarConfiguracion(nuevoNombreJugador, nuevoNumeroTiradas, nuevoDragonBallActivado)
         }
     }
 
-    private fun guardarConfiguracion(nombreJugador: String, numeroTiradas: Int) {
+    private fun guardarConfiguracion(nombreJugador: String, numeroTiradas: Int, dragonBallActivado: Boolean) {
         // Usar un editor para modificar SharedPreferences
         val editor = sharedPreferences.edit()
 
-        // Guardar el nuevo nombre de jugador y el número de tiradas
+        // Guardar el nuevo nombre de jugador, el número de tiradas y la configuración de Dragon Ball
         editor.putString("nombreJugador", nombreJugador)
         editor.putInt("numeroTiradas", numeroTiradas)
+        editor.putBoolean("dragonBallActivado", dragonBallActivado)
 
         // Aplicar los cambios
         editor.apply()
--- app/src/main/java/com/example/myapplication/DadosActivity.kt
+++ app/src/main/java/com/example/myapplication/DadosActivity.kt
@@ -39,18 +39,19 @@ class DadosActivity : AppCompatActivity() {
         // Utiliza la configuración obtenida de SharedPreferences
         val nombreJugador = sharedPreferences.getString("nombreJugador", "") ?: ""
         val numeroTiradas = sharedPreferences.getInt("numeroTiradas", 5)
+        val dragonBallActivado = sharedPreferences.getBoolean("dragonBallActivado", false)
 
         // Utiliza el nombre de jugador y el número de tiradas en lugar de valores fijos
-        scheduleRun(nombreJugador, numeroTiradas)
+        scheduleRun(nombreJugador, numeroTiradas, dragonBallActivado)
     }
 
-    private fun scheduleRun(nombreJugador: String, numeroTiradas: Int) {
+    private fun scheduleRun(nombreJugador: String, numeroTiradas: Int, dragonBallActivado: Boolean) {
         val schedulerExecutor = Executors.newSingleThreadScheduledExecutor()
         val msc = 1000
         for (i in 1..numeroTiradas) {
             schedulerExecutor.schedule(
                 {
-                    throwDadoInTime()
+                    throwDadoInTime(dragonBallActivado)
                 },
                 msc * i.toLong(), TimeUnit.MILLISECONDS
             )
@@ -58,13 +59,13 @@ class DadosActivity : AppCompatActivity() {
 
         schedulerExecutor.schedule({
             viewResult(nombreJugador)
-        }, msc * (numeroTiradas + 2).toLong(), TimeUnit.MILLISECONDS)
+        }, msc * (numeroTiradas + 5).toLong(), TimeUnit.MILLISECONDS)
 
         schedulerExecutor.shutdown()
     }
 
-    private fun throwDadoInTime() {
-        val numDados = Array(3) { Random.nextInt(1, 6) }
+    private fun throwDadoInTime(dragonBallActivado: Boolean) {
+        val numDados = Array(3) { Random.nextInt(1, 7) }
         val imageViews: Array<ImageView> = arrayOf(
             bindingDados.imagviewDado1,
             bindingDados.imagviewDado2,
@@ -73,18 +74,31 @@ class DadosActivity : AppCompatActivity() {
 
         sum = numDados.sum()
         for (i in 0 until 3) {
-            selectView(imageViews[i], numDados[i])
+            selectView(imageViews[i], numDados[i], dragonBallActivado)
         }
     }
 
-    private fun selectView(imgV: ImageView, v: Int) {
-        when (v) {
-            1 -> imgV.setImageResource(R.drawable.dado1)
-            2 -> imgV.setImageResource(R.drawable.dado2)
-            3 -> imgV.setImageResource(R.drawable.dado3)
-            4 -> imgV.setImageResource(R.drawable.dado4)
-            5 -> imgV.setImageResource(R.drawable.dado5)
-            6 -> imgV.setImageResource(R.drawable.dado6)
+    private fun selectView(imgV: ImageView, imagenes: Int, dragonBallActivado: Boolean) {
+        if (dragonBallActivado) {
+            // Si Dragon Ball está activado, utiliza las imágenes de bolas de Dragon Ball
+            when (imagenes) {
+                1 -> imgV.setImageResource(R.drawable.dg1)
+                2 -> imgV.setImageResource(R.drawable.dg2)
+                3 -> imgV.setImageResource(R.drawable.dg3)
+                4 -> imgV.setImageResource(R.drawable.dg4)
+                5 -> imgV.setImageResource(R.drawable.dg5)
+                6 -> imgV.setImageResource(R.drawable.dg1)
+            }
+        } else {
+            // Si Dragon Ball no está activado, utiliza las imágenes de dados normales
+            when (imagenes) {
+                1 -> imgV.setImageResource(R.drawable.dado1)
+                2 -> imgV.setImageResource(R.drawable.dado2)
+                3 -> imgV.setImageResource(R.drawable.dado3)
+                4 -> imgV.setImageResource(R.drawable.dado4)
+                5 -> imgV.setImageResource(R.drawable.dado5)
+                6 -> imgV.setImageResource(R.drawable.dado6)
+            }
         }
     }
 
