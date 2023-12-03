6ec29f94d87cbca61d64d9fc6e94a333c20ff194
Pedro <pfernandezgonzalez@vva.safanet.es>
2023-12-03 11:24:21 +0100

    Versión 0.2 - Funcionalidad contar chiste incluida

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.example.myapplication
+
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.SystemClock
 import android.provider.AlarmClock
+import android.speech.tts.TextToSpeech
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.Locale
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var imageView: ImageView
+    private lateinit var textToSpeech: TextToSpeech
+
+    private var isSpeaking = false
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +36,10 @@ class MainActivity : AppCompatActivity() {
         val botonWeb = findViewById<Button>(R.id.google)
         val botonAlarma = findViewById<Button>(R.id.alarma)
         val botonCaptura = findViewById<Button>(R.id.captura)
+        val botonChiste = findViewById<Button>(R.id.chiste)
         imageView = findViewById(R.id.imageView2)
+
+        configureTextToSpeech()
 
         botonLlamada.setOnClickListener {
             val llamadaIntent = Intent(this, LlamadaActivity::class.java)
@@ -36,33 +51,30 @@ class MainActivity : AppCompatActivity() {
             val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             startActivity(webIntent)
         }
-
         botonAlarma.setOnClickListener {
             val calendar = java.util.Calendar.getInstance()
             val horaActual = calendar.get(java.util.Calendar.HOUR_OF_DAY)
             val minutosActuales = calendar.get(java.util.Calendar.MINUTE)
 
-            // Sumar 2 minutos
             val minutosAlarma = minutosActuales + 3
             val horaAlarma = horaActual + minutosAlarma / 60
             val minutosFinal = minutosAlarma % 60
 
-            val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
-                putExtra(AlarmClock.EXTRA_HOUR, horaAlarma)
-                putExtra(AlarmClock.EXTRA_MINUTES, minutosFinal)
-                putExtra(AlarmClock.EXTRA_SKIP_UI, true) // Omitir la interfaz de usuario de la aplicación de reloj
-                putExtra(AlarmClock.EXTRA_MESSAGE, "Alarma")
-                putExtra(AlarmClock.EXTRA_VIBRATE, true)
-                putExtra(AlarmClock.EXTRA_RINGTONE, "content://settings/system/alarm_alert")
-            }
-
-            if (intent.resolveActivity(packageManager) != null) {
+            try {
+                val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+                    putExtra(AlarmClock.EXTRA_HOUR, horaAlarma)
+                    putExtra(AlarmClock.EXTRA_MINUTES, minutosFinal)
+                    putExtra(AlarmClock.EXTRA_SKIP_UI, true)
+                    putExtra(AlarmClock.EXTRA_MESSAGE, "Alarma")
+                    putExtra(AlarmClock.EXTRA_VIBRATE, true)
+                    putExtra(AlarmClock.EXTRA_RINGTONE, "content://settings/system/alarm_alert")
+                }
                 startActivity(intent)
-            } else {
-                Toast.makeText(this, "La aplicación de reloj no está disponible en este dispositivo", Toast.LENGTH_LONG).show()
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(this, "Error al configurar la alarma", Toast.LENGTH_SHORT).show()
             }
         }
-
 
         botonCaptura.setOnClickListener {
             val captura = captureScreen()
@@ -70,9 +82,40 @@ class MainActivity : AppCompatActivity() {
                 imageView.setImageBitmap(captura)
                 Toast.makeText(this, "Captura pantalla hecha", Toast.LENGTH_LONG).show()
             } else {
-                Toast.makeText(this, "No he podido tomar la captura de pantalla", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    this,
+                    "No he podido tomar la captura de pantalla",
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
+
+        botonChiste.setOnClickListener {
+            if (!isSpeaking) {
+                val chistesArray = resources.getStringArray(R.array.chistes)
+                val randomIndex = Random.nextInt(chistesArray.size)
+                val chisteSeleccionado = chistesArray[randomIndex]
+                speakMeDescription(chisteSeleccionado)
+
+                // Simulamos un tiempo de reproducción del chiste (ajusta según sea necesario)
+                GlobalScope.launch(Dispatchers.Main) {
+                    delay(600)  // Ajusta el tiempo de reproducción del chiste
+                    isSpeaking = false
+                }
+            }
+        }
+    }
+
+    private fun configureTextToSpeech() {
+        textToSpeech = TextToSpeech(applicationContext, TextToSpeech.OnInitListener {
+            if (it != TextToSpeech.ERROR) {
+                textToSpeech.language = Locale.getDefault()
+            }
+        })
+    }
+
+    private fun speakMeDescription(s: String) {
+        textToSpeech.speak(s, TextToSpeech.QUEUE_FLUSH, null, null)
     }
 
     private fun captureScreen(): Bitmap? {
@@ -81,5 +124,13 @@ class MainActivity : AppCompatActivity() {
         val captura = Bitmap.createBitmap(vista.drawingCache)
         vista.isDrawingCacheEnabled = false
         return captura
+    }
+
+    override fun onDestroy() {
+        if (::textToSpeech.isInitialized) {
+            textToSpeech.stop()
+            textToSpeech.shutdown()
+        }
+        super.onDestroy()
     }
 }
